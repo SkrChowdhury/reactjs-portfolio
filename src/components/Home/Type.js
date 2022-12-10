88065930a66b55ee28141c6651e87af4c2c5acb8
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-12-10 19:05:28 +0600

    react js portfolio created

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          'Full Stack Developer',
+          'Mobile App Developer',
+          'MERN Stack Developer',
+          'Open Source Contributor',
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
