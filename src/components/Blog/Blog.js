8731b2c5fd34e72d4a121ca4e56df87aeaca9c06
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-12-12 00:19:15 +0600

    blog updated

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const Blog = () => {
   return (
-    <div>
-      <p className="p-5 m-5 fs-5 text-white">Coming Soon</p>
+    <div className="mt-5 pt-5 ">
+      <Player
+        src="https://assets2.lottiefiles.com/packages/lf20_m6cuL6.json"
+        autoplay
+        loop
+        style={{
+          width: '80%',
+          height: '100%',
+          alignSelf: 'center',
+          justifySelf: 'center ',
+          marginTop: '1%',
+        }}
+      />
     </div>
   );
 };
